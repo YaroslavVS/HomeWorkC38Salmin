8ec1a83341574afbc2597eccbc60ca0b8951d789
YaroslavVS <yar.salmin0789@gmail.com>
2025-10-12 20:36:12 +0700

    Сделаны 1 и 2 задача.

--- src/HomeWork1/Main.java
+++ src/HomeWork1/Main.java
@@ -2,6 +2,23 @@ package HomeWork1;
 
 public class Main {
     public static void main(String[] args) {
+        calculateTask1();
+        calculateTask2();
 
     }
+
+    public static void calculateTask1() {
+        System.out.println("Иван Иванов");
+    }
+
+    public static void calculateTask2() {
+        int age;
+        age = 36;
+        System.out.println("Иван Иванов " + age);
+    }
+
+
+
+
+
 }
