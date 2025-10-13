27404ab801c671a0776f3ce044dad49044a52034
YaroslavVS <yar.salmin0789@gmail.com>
2025-10-13 17:59:58 +0700

    Добавил названия самих задач и разделил их, в общем тренирую работу с гитом.

--- src/HomeWorkBlock1/Main.java
+++ src/HomeWorkBlock1/Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
         // Задача 1:
         //Создать программу, которая будет выводиться в консоль вашего имени и семьи
+        System.out.println("________________________");
+        System.out.println("Урок1, Блок1, Задача 1:");
         calculateTask1();
+        System.out.println("________________________");
         //Задача 2:
         //Добавить в программу целочисленную переменную с названием возраст, в котором будет
         //сохранить свой возраст. Выведите в консоли свое имя, фамилию и возраст
+        System.out.println("Урок1, Блок1, Задача 2:");
         calculateTask2();
+        System.out.println("________________________");
     }
 
     public static void calculateTask1() {
